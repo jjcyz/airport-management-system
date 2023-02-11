0c86cdb65086d0dce6880ba0449a4c955a3e9346
Jessica Zhou <jess.c.zhou@gmail.com>
2023-02-11 14:48:44 -0800

    last test

--- src/test/model/FlightTest.java
+++ src/test/model/FlightTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FlightTest {
     private Passenger p1;
     private Passenger p2;
+    private Passenger p3;
     private Flight testFlight1;
     private Aircraft testAircraft1;
 
@@ -17,8 +18,9 @@ public class FlightTest {
     void runBefore() {
         p1 = new Passenger(123,"Ada","Lovelace",TravelClasses.FIRSTCLASS);
         p2 = new Passenger(456,"Alan","Turing",TravelClasses.FIRSTCLASS);
+        p3 = new Passenger(789, "Charles", "Babbage", TravelClasses.FIRSTCLASS);
 
-        testAircraft1 = new Aircraft("ID12345", 1);
+        testAircraft1 = new Aircraft("ID12345", 2);
 
         testFlight1 = new Flight("ID12345", testAircraft1, YVR,Airports.YYZ,4);
 
@@ -42,15 +44,19 @@ public class FlightTest {
 
     @Test
     void addPassengerTest() {
-        assertEquals("Ada Lovelace has been added to this flight",testFlight1.addPassenger(p1) );
+        assertEquals("Ada Lovelace has been added to this flight",testFlight1.addPassenger(p1));
         assertTrue(testFlight1.isPassengerOnFlight(123));
         assertFalse(testFlight1.isPassengerOnFlight(999));
-        assertEquals("The aircraft for this flight is at maximum capacity.",testFlight1.addPassenger(p2));
-        assertFalse(testFlight1.isPassengerOnFlight(456));
+        assertEquals("Alan Turing has been added to this flight",testFlight1.addPassenger(p2));
+        assertEquals("The aircraft for this flight is at maximum capacity.",testFlight1.addPassenger(p3));
+        assertTrue(testFlight1.isPassengerOnFlight(456));
+        assertFalse(testFlight1.isPassengerOnFlight(789));
+        assertFalse(testFlight1.isPassengerOnFlight(888));
     }
 
     @Test
     void removePassengerTest() {
+        testFlight1.addPassenger(p2);
         testFlight1.addPassenger(p1);
         assertTrue(testFlight1.isPassengerOnFlight(123));
         int before = testFlight1.getListOfPassengers().size();
@@ -77,9 +83,9 @@ public class FlightTest {
 
     @Test
     void getAvailableSeats() {
-        assertEquals(1, testFlight1.getAvailableSeats());
+        assertEquals(2, testFlight1.getAvailableSeats());
         testFlight1.addPassenger(p1);
-        assertEquals(0, testFlight1.getAvailableSeats());
+        assertEquals(1, testFlight1.getAvailableSeats());
     }
 
     @Test
@@ -100,7 +106,7 @@ public class FlightTest {
     void toStringTest() {
         assertEquals("Flight ID: ID12345 Aircraft: " +
                 "Name of Aircraft: ID12345 Maximum " +
-                "Capacity: 1 Origin: YVR Destination: " +
+                "Capacity: 2 Origin: YVR Destination: " +
                 "YYZ Duration: 4", testFlight1.toString());
     }
 }
